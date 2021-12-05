dc3e7345798740d4199402945578d658c577e1ae
seoyeon-jung <jungluchia1204@gmail.com>
2021-12-05 23:01:22 +0900

    try week2 (not finish)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ProductList from './pages/ProductList';
+import ProductDetail from './pages/ProductDetail';
+import Lists from './pages/Lists';
+import Error from './pages/Error';
+
+class App extends Component {
+  render () {
+    return (
+      <Router basename="/react-shopping-webapp">
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/ProductDetail" element={<ProductDetail />} />
+          <Route path="/Lists" element={<Lists />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/apis/config.js
@@ -0,0 +1,2 @@
+export const url =
+'https://go.postman.co/workspace/My-Workspace~cea756b5-2960-4725-8208-03e229bfd504/collection/18655058-ffd3783b-357a-4c67-9339-ff94c28fdc42';
